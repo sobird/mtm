809b6e902165a0a6ff9cb7124099996c0d902df0
sobird <i@sobird.me>
2023-06-02 01:07:26 +0800

    output.clean

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ const config = {
     filename: '[name].[contenthash].js',
     chunkFilename: '[name].[contenthash].chunk.js',
     publicPath: '/',
+    clean: true,
   },
   devServer: {
     open: true,
