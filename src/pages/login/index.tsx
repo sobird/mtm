4f5a5f0f819c266dcb6c3e4c835220673e9b6480
sobird <i@sobird.me>
2023-06-13 19:47:57 +0800

    登录页面第二版

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,28 +1,31 @@
 /**
  * 用户登录页面
- * 
+ *
  * sobird<i@sobird.me> at 2023/06/12 8:24:06 created.
  */
 import React, { useEffect } from 'react';
 import { useNavigate, RouterProviderProps, useLocation } from 'react-router-dom';
 import queryString from 'query-string';
 import './index.scss';
-import mtLogo from '@/assets/mt_logo.png';
-import thhLogo from '@/assets/thh_logo.png';
-import thhLoginBg from '@/assets/thh_login_bg.png';
+import mtmLogo from '@/assets/mtm_logo.png';
+import mtmLoginBg from '@/assets/mtm_login_bg.png';
+import policeIcon from '@/assets/police_icon.png';
 
 function Login() {
   const navigate = useNavigate();
   const location = useLocation();
-  console.log('location', location)
+  console.log('location', location);
   useEffect(() => {
     try {
       // 短时间多次登录处理逻辑
       const preLoginDateStr = localStorage.getItem('preLoginDateStr') || '';
       const preLoginDateArr = preLoginDateStr.split(',');
       // 最近10秒连续登陆3次，最近1分钟连续登陆5次，跳转到error，并进行上报
-      if ((preLoginDateArr.length >= 3 && +preLoginDateArr[0] - +preLoginDateArr[2] < 10000) || (preLoginDateArr.length >= 5 && +preLoginDateArr[0] - +preLoginDateArr[4] < 60000)) {
-        // 
+      if (
+        (preLoginDateArr.length >= 3 && +preLoginDateArr[0] - +preLoginDateArr[2] < 10000) ||
+        (preLoginDateArr.length >= 5 && +preLoginDateArr[0] - +preLoginDateArr[4] < 60000)
+      ) {
+        //
         localStorage.setItem('preLoginDateStr', '');
         navigate(`/error?message=${encodeURIComponent('您的店铺登录遇到问题，请切换账号再尝试')}`);
       }
@@ -33,10 +36,11 @@ function Login() {
   // 补签协议的 case 增加特殊的scene，只牵涉到登陆和忘记密码
   const { search } = location;
   const query = queryString.parse(search);
-  const from = query && query.from;
+  const from = query?.from;
   const isSupply = from === '/supplementSignContract';
-  let loginContinue = `${process.env.FMA_EPASSPORT_SIGN_CONTINUE_URL
-    || window.location.origin}/afterlogin?scene=platform${isSupply ? '-contract' : ''}-login&from=${from}`;
+  let loginContinue = `${
+    process.env.FMA_EPASSPORT_SIGN_CONTINUE_URL || window.location.origin
+  }/afterlogin?scene=platform${isSupply ? '-contract' : ''}-login&from=${from}`;
   let recContinue = `${process.env.FMA_EPASSPORT_CONTINUE_URL}?scene=platform${isSupply ? '-contract' : ''}-recover`;
 
   loginContinue = encodeURIComponent(loginContinue);
@@ -49,29 +53,20 @@ function Login() {
   // 需要特殊申请一个配置，注册按钮设置为链接方式并且打开新页面
 
   return (
-    <div className="login">
-      <div className="login-header">
-        <div className="logo-box">
-          <img src={mtLogo} alt="" className="mt-logo" />
-          <img
-            src={thhLogo}
-            alt=""
-            className="thh-logo"
-          />
+    <div className='login'>
+      <div className='login-header'>
+        <div className='logo-box'>
+          <img src={mtmLogo} />
         </div>
       </div>
-      <div className="login-body">
-        <div className="login-main">
-          <img
-            src={thhLoginBg}
-            alt=""
-            className="bg"
-          />
-          <div className="login-window">
-            <div className="login-title">登录</div>
-            <div className="iframe-container">
+      <div className='login-body'>
+        <div className='login-main'>
+          <img src={mtmLoginBg} className='bg' />
+          <div className='login-window'>
+            <div className='login-title'>登录</div>
+            <div className='iframe-container'>
               <iframe
-                title="美团团好货"
+                title='美团电商'
                 src={loginUrl}
                 // sandbox="allow-forms allow-scripts allow-top-navigation allow-same-origin"
               />
@@ -79,11 +74,24 @@ function Login() {
           </div>
         </div>
       </div>
-      <div className="login-footer">
-        <p className="copy">©2020 meituan.com 京ICP证070791号 京公网安备11010502025545号</p>
+      <div className='login-footer'>
+        <div className='contact-reword'>
+          <a target='_blank' href='https://beian.miit.gov.cn' rel='noreferrer' style={{ marginTop: '3px' }}>
+            <span>©2020 meituan.com 京ICP备10211739号</span>
+          </a>
+          <div />
+          <img src={policeIcon} style={{ marginRight: '3px' }} />
+          <a
+            target='_blank'
+            href='http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11000002002052'
+            rel='noreferrer'
+          >
+            <span>京公网安备11000002002052号</span>
+          </a>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Login;
