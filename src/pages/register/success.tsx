c00f752eb8e9c2b208c4148bb5b8c40331fede19
sobird <i@sobird.me>
2023-06-20 21:47:38 +0800

    登录注册样式优化

--- src/pages/register/success.tsx
+++ src/pages/register/success.tsx
@@ -4,7 +4,7 @@
  * sobird<i@sobird.me> at 2023/06/20 12:47:45 created.
  */
 
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Base from "@/components/layout/base";
 import { Result, Button } from 'antd'
 
