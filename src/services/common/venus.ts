ce4ed6a39eaf97a052dbbb76dc7a8842c7d2ddc2
sobird <i@sobird.me>
2023-08-16 02:25:38 +0800

    VenusUpload

--- src/services/common/venus.ts
+++ src/services/common/venus.ts
@@ -13,33 +13,48 @@ interface IVenusSignature {
   bucket: string;
 }
 
+interface IVenusUpload {
+  md5: string;
+  key: string;
+  url: string;
+  name: string;
+  size: number;
+  height: number;
+  width: number;
+  format: string;
+}
+
 export function signature() {
   return http.get<IVenusSignature>('/venus/sign');
 }
 
-export async function upload(file: File) {
-  const sign = await signature();
+export async function upload(file: File, progress: (p: number) => void) {
+  const { token, bucket} = await signature();
 
-  const key = `${sign.bucket}${nanoid()}-${file.name}`;
+  const key = `${bucket}${nanoid()}-${file.name}`;
 
   const formData = new FormData();
   formData.append('key', key);
   formData.append('file', file);
 
-  return http.service({
+  return http.service<IVenusUpload>({
     method: 'post',
     url: `/venus/upload`,
     headers: {
-      Authorization: sign.token,
+      Authorization: token,
     },
     data: formData,
-    responseType: 'blob',
+    // responseType: 'blob',
+    onUploadProgress(progressEvent) {
+      console.log('progressEvent', progressEvent)
+      progress && progress(Math.floor(progressEvent.loaded / progressEvent.total * 100))
+    }
   });
 }
 
 const VenusServices = {
   signature,
-  upload
+  upload,
 }
 
 export default VenusServices;
