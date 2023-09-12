9dcf2962960040f5483ef34ee23c37d7a3bcb762
sobird <i@sobird.me>
2023-09-12 16:02:04 +0800

    Aside组件

---
+++ src/components/layout/main/aside/index.tsx
@@ -0,0 +1,38 @@
+/**
+ * 侧边栏导航组件
+ * 
+ * sobird<i@sobird.me> at 2023/09/12 15:46:42 created.
+ */
+
+import React from "react";
+import { Button, Menu } from "antd";
+import { AppstoreOutlined, SettingOutlined, MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+
+import "./index.scss";
+
+const Aside: React.FunctionComponent = () => {
+  return (
+    <aside className="app-aside">
+      <div className="app-menu">
+        <Menu
+          mode="inline"
+          //inlineCollapsed={collapsed}
+          defaultSelectedKeys={[location.pathname]}
+        >
+          <Menu.Item key="/" icon={<AppstoreOutlined />}>
+            <Link to="/">首页</Link>
+          </Menu.Item>
+          <Menu.Item
+            key="/about"
+            icon={<SettingOutlined />}
+          >
+            <Link to="/about">关于</Link>
+          </Menu.Item>
+        </Menu>
+      </div>
+    </aside>
+  )
+}
+
+export default Aside;
--- src/components/layout/main/index.tsx
+++ src/components/layout/main/index.tsx
@@ -5,15 +5,38 @@
  */
 import classNames from 'classnames';
 import Header from './header';
+import Aside from './aside';
 
 export default function LayoutMain() {
   return (
-    <div 
-      className={classNames('app-container', {
-        mounted: true,
-      })}
-    >
-      <Header />
-    </div>
+    <>
+      <div 
+        className={classNames('app-container', {
+          mounted: true,
+        })}
+      >
+        <Header />
+        <div className="app-body">
+          <Aside />
+          <main className="app-main">
+
+          </main>
+        </div>
+      </div>
+      <div
+        className="app-watermark"
+        style={{
+          backgroundImage: 'url(/api-wm/image/visible?deg=-20&type=4)',
+          backgroundSize: '330px, auto',
+          position: 'fixed',
+          left: 0,
+          top: 0,
+          width: '100%',
+          height: '100%',
+          zIndex: 999999,
+          pointerEvents: 'none',
+        }}
+      />
+    </>
   )
 }
