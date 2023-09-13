f98d87e8ca4e67f75c340591da7ef13e9620adf0
sobird <i@sobird.me>
2023-09-13 17:49:22 +0800

    hooks useIntersectionObserver

---
+++ src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,67 @@
+/**
+ * useIntersectionObserver.ts
+ * 
+ * @see https://developer.mozilla.org/zh-CN/docs/Web/API/IntersectionObserver
+ * 
+ * sobird<i@sobird.me> at 2023/09/13 17:29:43 created.
+ */
+
+import React, { useEffect, useRef, PropsWithChildren } from "react";
+
+interface IUseIntersectionObserverProps {
+  options?: object;
+  threshold?: number;
+  /**
+   * 需要被监听的元素ID
+   */
+  id: string;
+  callback: () => void;
+}
+
+const useIntersectionObserver: React.FC<PropsWithChildren<IUseIntersectionObserverProps>> = (
+  { options = {}, threshold = 0.1, id, callback, children }
+) => {
+  const observer = useRef<null | IntersectionObserver>();
+
+  useEffect(() => {
+    const target = document.getElementById(id);
+    if(!id) {
+      return;
+    }
+
+    const Options = {
+      ...options,
+      // 表示重叠面积占被观察者的比例，从 0 - 1 取值，1 表示完全被包含
+      threshold,
+    };
+
+    const CallBack = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach(entry => {
+        const radio = entry.intersectionRatio;
+        const rect = entry.boundingClientRect;
+        if (radio >= threshold && rect.y > 0) {
+          const reported = target.getAttribute('reported');
+          if (reported !== 'true') {
+            callback && callback();
+            target.setAttribute('reported', 'true');
+          }
+        }
+      });
+    };
+
+    observer.current = new IntersectionObserver(CallBack, Options);
+    const ob = observer.current;
+    if (target) {
+      ob.observe(target);
+    }
+
+    return () => {
+      observer?.current?.unobserve && target && observer.current.unobserve(target);
+    };
+
+  }, [options, threshold, id, callback])
+
+  return children;
+}
+
+export default useIntersectionObserver;
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,15 @@
+/**
+ * 首页
+ * 
+ * sobird<i@sobird.me> at 2023/09/13 17:24:00 created.
+ */
+
+import React from "react"
+
+const Home: React.FC = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Home;
