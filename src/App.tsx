a3c1f8b1669f08db4ccf26039ab687e17bf10054
sobird <i@sobird.me>
2023-09-13 22:43:14 +0800

    @mtm/common

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import EntryCompany from './pages/settleinjx/company';
 import LayoutMain from './components/layout/main';
 import './App.css';
 
-import '../packages/common/provider';
+import '@mtm/common/provider';
 
 const theme = { token: { colorPrimary: '#ffd100', borderRadius: 2, colorWhite: '#333' } };
 
