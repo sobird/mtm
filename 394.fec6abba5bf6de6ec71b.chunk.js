244d8b28cb732acf9ca008b4fc7b52994576dd23
sobird <i@sobird.me>
2023-10-08 14:40:07 +0000

    Deploying to gh-pages from @ sobird/mtm@7b2c677d2841af2ad5529d6b6f0383dba2d2eacd 🚀

--- 394.fec6abba5bf6de6ec71b.chunk.js
+++
@@ -1,3 +0,0 @@
-"use strict";(window.webpackJsonp_mtm=window.webpackJsonp_mtm||[]).push([[394],{71394:(m,E,t)=>{t.r(E),t.d(E,{default:()=>o});var s=t(85893),n=t(67294),_=t(95998);const o=()=>{const c=(0,_.v9)(d=>d.app);return(0,n.useEffect)(()=>{},[]),(0,s.jsx)("div",{children:"home"})}}}]);
-
-//# sourceMappingURL=/394.fec6abba5bf6de6ec71b.chunk.js.map
