414dc4afc32bb3e69851a29f1cf63b3f38b1b098
sobird <i@sobird.me>
2023-10-12 17:07:27 +0800

    组件: View

---
+++ packages/mtm_market/src/remotes/workbench/components/card/View.tsx
@@ -0,0 +1,48 @@
+/**
+ * View 业务级组件
+ *
+ * sobird<i@sobird.me> at 2023/10/12 16:01:11 created.
+ */
+
+import React from 'react';
+import classNames from 'classnames';
+
+export interface CardViewProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'title'> {
+  prefixCls?: string;
+  className?: string;
+  hoverable?: boolean;
+  style?: React.CSSProperties;
+  title: React.ReactNode;
+  unit?: string;
+  value: React.ReactNode;
+  extra?: React.ReactNode;
+  bordered?: true;
+}
+
+const View: React.FC<CardViewProps> = ({
+  prefixCls = 'mix',
+  className,
+  hoverable = true,
+  title,
+  value,
+  unit,
+  extra,
+  bordered,
+  ...props
+}) => {
+  const classString = classNames(`${prefixCls}-card-view`, className, {
+    [`${prefixCls}-card-view-bordered`]: bordered,
+  });
+  return (
+    <div {...props} className={classString}>
+      <div className={`${prefixCls}-card-view-title`}>{title}</div>
+      <div className={`${prefixCls}-card-view-content`}>
+        <span className={`${prefixCls}-card-view-value`}>{value || "--"}</span>
+        <span className={`${prefixCls}-card-view-unit`}>{unit}</span>
+      </div>
+      <div className={`${prefixCls}-card-view-extra`}>{extra}</div>
+    </div>
+  );
+};
+
+export default View;
--- packages/mtm_market/src/remotes/workbench/realdata/index.tsx
+++ packages/mtm_market/src/remotes/workbench/realdata/index.tsx
@@ -4,10 +4,10 @@
  * sobird<i@sobird.me> at 2023/10/12 9:23:24 created.
  */
 import Card from '../components/card';
+import { Tooltip } from 'antd';
 
 const gridStyle: React.CSSProperties = {
   width: '25%',
-  textAlign: 'center',
 };
 
 const RealData = () => {
@@ -16,13 +16,38 @@ const RealData = () => {
       title='实时数据'
       subTitle='数据计算可能会存在5-10分钟左右的延迟'
       extra={<a>数据中心</a>}
-      bodyStyle={{
-        height: 90
-      }}
+      bodyStyle={
+        {
+          // height: 90
+        }
+      }
     >
-      <Card.Grid style={gridStyle}>实时数据<div>ddd</div></Card.Grid>
-      <Card.Grid style={gridStyle}>实时数据<div>ddd</div></Card.Grid>
-      <Card.Grid style={gridStyle}>实时数据<div>ddd</div></Card.Grid>
+      <Card.Grid style={gridStyle}>
+        <Card.View
+          title={
+            <Tooltip title={<span style={{ color: '#333', fontSize: 12 }}>所有当前进入支付页但未支付的订单数</span>} color='#fff'>
+              <span>待付款</span>
+            </Tooltip>
+          }
+          unit='元'
+          value={23}
+          extra={<a>如何提高转化率</a>}
+        ></Card.View>
+      </Card.Grid>
+      <Card.Grid style={gridStyle}>
+        <Card.View
+          title={
+            <Tooltip title={<span style={{ color: '#333', fontSize: 12 }}>近90天内的所有待发货订单数</span>} color='#fff'>
+              <span>待发货</span>
+            </Tooltip>
+          }
+          unit='单'
+          value={68}
+        ></Card.View>
+      </Card.Grid>
+      <Card.Grid style={gridStyle}>
+        实时数据<div>ddd</div>
+      </Card.Grid>
     </Card>
   );
 };
